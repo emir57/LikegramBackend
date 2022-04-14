13f926b0d2dea0dc3fde47fda56ee7b2dcc95c37
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:14:00 +0300

    BaseEntity

---
+++ Likegram.Entities/Concrete/PostLike.cs
@@ -0,0 +1,19 @@
+﻿using Likegram.Core.Entities;
+using Likegram.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class PostLike : BaseEntity
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
