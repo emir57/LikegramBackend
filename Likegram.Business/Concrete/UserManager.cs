fa54aa65eba410df538625df707d7e1a7bd9fd59
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 20:19:54 +0300

    BusinessMessages

---
+++ Likegram.Business/Concrete/UserManager.cs
@@ -0,0 +1,62 @@
+﻿using Likegram.Business.Abstract;
+using Likegram.Core.Entities.Concrete;
+using Likegram.Core.Utilities.Result;
+using Likegram.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public async Task<IResult> Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<User>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<User>> GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<User>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<User>> GetByUsername(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<User>>> UserRoles(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Likegram.Business/Constants/BusinessMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Business.Constants
+{
+    public class BusinessMessages
+    {
+        public static string EklemeBasarili = "Ekleme Başarılı";
+        public static string EklemeBasarisiz = "Ekleme Başarısız";
+
+        public static string GuncellemeBasarili = "Güncelleme Başarılı";
+        public static string GuncellemeBasarisiz = "Güncelleme Başarısız";
+
+        public static string SilmeBasarili = "Silme Başarılı";
+        public static string SilmeBasarisiz = "Silme Başarısız";
+
+        public static string ListelemeBasarili = "Listeleme Başarılı";
+        public static string GetirmeBasarili = "Getirme Başarılı";
+    }
+}
