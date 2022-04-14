6421fdb847185f099cd49ebc3e4c65882b2c443d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:11:50 +0300

    PostComment Entity

---
+++ Likegram.Entities/Concrete/PostComment.cs
@@ -0,0 +1,17 @@
+﻿using Likegram.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class PostComment : BaseEntity
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string Comment { get; set; }
+        public List<CommentAnswer> CommentAnswers { get; set; }
+    }
+}
