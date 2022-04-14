7defe546283fd4692ddeca4911a4341dac8dadac
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:30:27 +0300

    AuthManager

--- Likegram.Business/Concrete/AuthManager.cs
+++ Likegram.Business/Concrete/AuthManager.cs
@@ -1,4 +1,8 @@
 ﻿using Likegram.Business.Abstract;
+using Likegram.Core.Entities.Concrete;
+using Likegram.Core.Entities.Dtos;
+using Likegram.Core.Utilities.Result;
+using Likegram.Core.Utilities.Security.JWT;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,21 @@ using System.Threading.Tasks;
 
 namespace Likegram.Business.Concrete
 {
-    public class AuthManager: IAuthService
+    public class AuthManager : IAuthService
     {
+        public Task<IDataResult<AccessToken>> CreateToken(User user, List<Role> roles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<User>> Login(UserForLoginDto userForLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<User>> Register(UserForRegisterDto userForRegisterDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
