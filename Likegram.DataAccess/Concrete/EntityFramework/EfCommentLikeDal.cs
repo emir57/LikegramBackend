15aee10d96ca7a07597645fda6541cdc67bb09a1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:00:26 +0300

    EfCommentLikeDal

---
+++ Likegram.DataAccess/Concrete/EntityFramework/EfCommentLikeDal.cs
@@ -0,0 +1,16 @@
+﻿using Likegram.Core.DataAccess.Entityframework;
+using Likegram.DataAccess.Abstract;
+using Likegram.DataAccess.Contexts;
+using Likegram.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.DataAccess.Concrete.EntityFramework
+{
+    public class EfCommentLikeDal : EFEntityRepostioryBase<CommentLike, LikegramDbContext>, ICommentLikeDal
+    {
+    }
+}
