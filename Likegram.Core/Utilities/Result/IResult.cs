1fefd22b4c85cedf6d05a0c80029dfb253ba51cc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:16:45 +0300

    IResult

--- Likegram.Core/Utilities/Result/IResult.cs
+++ Likegram.Core/Utilities/Result/IResult.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Likegram.Business.Abstract
+namespace Likegram.Core.Utilities.Result
 {
-    public interface IAuthService
+    public interface IResult
     {
-
+        string Message { get; }
+        bool Success { get; }
     }
 }
