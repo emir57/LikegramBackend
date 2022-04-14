411b02aba74d7f0dbde0463fa585d19a124a9923
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:53:23 +0300

    EfUserRoleDal

---
+++ Likegram.DataAccess/Concrete/EntityFramework/EfUserRoleDal.cs
@@ -0,0 +1,16 @@
+﻿using Likegram.Core.DataAccess.Entityframework;
+using Likegram.Core.Entities.Concrete;
+using Likegram.DataAccess.Abstract;
+using Likegram.DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserRoleDal : EFEntityRepostioryBase<UserRole, LikegramDbContext>, IUserRoleDal
+    {
+    }
+}
