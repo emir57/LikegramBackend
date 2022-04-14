649f2b9c44914851bc4db17387f55dd1690f6dc6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:12:50 +0300

    CommentAnswer Entity

---
+++ Likegram.Entities/Concrete/CommentAnswer.cs
@@ -0,0 +1,16 @@
+﻿using Likegram.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class CommentAnswer : BaseEntity
+    {
+        public int CommentId { get; set; }
+        public int UserId { get; set; }
+        public string Answer { get; set; }
+    }
+}
