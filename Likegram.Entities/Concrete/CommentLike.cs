7e99a64d8a8b7304366ca2c7315695b34a05a831
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:15:48 +0300

    CommentLike

---
+++ Likegram.Entities/Concrete/CommentLike.cs
@@ -0,0 +1,19 @@
+﻿using Likegram.Core.Entities;
+using Likegram.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class CommentLike : BaseEntity
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int CommentId { get; set; }
+        public PostComment PostComment { get; set; }
+    }
+}
