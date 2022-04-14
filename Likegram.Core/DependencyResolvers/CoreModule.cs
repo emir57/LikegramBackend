0dfa347c5066f7274e41489811f7cc881b79dece
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:01:03 +0300

    CoreModule add Stopwatch

--- Likegram.Core/DependencyResolvers/CoreModule.cs
+++ Likegram.Core/DependencyResolvers/CoreModule.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace Likegram.Core.DependencyResolvers
     {
         public void Load(IServiceCollection services)
         {
+            services.AddSingleton<Stopwatch>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
     }
