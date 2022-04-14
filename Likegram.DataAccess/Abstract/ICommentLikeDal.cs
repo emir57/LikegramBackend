814b6f5493fe77ad5079090b1ef8731cb2f9f53f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:59:37 +0300

    ICommentLikeDal

---
+++ Likegram.DataAccess/Abstract/ICommentLikeDal.cs
@@ -0,0 +1,14 @@
+﻿using Likegram.Core.DataAccess;
+using Likegram.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.DataAccess.Abstract
+{
+    public interface ICommentLikeDal : IEntityRepository<CommentLike>
+    {
+    }
+}
