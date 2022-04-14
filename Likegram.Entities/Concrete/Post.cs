e7ae90dbb0ffd883ce03103da0071387abe7334a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 18:16:39 +0300

    Refactoring Post

--- Likegram.Entities/Concrete/Post.cs
+++ Likegram.Entities/Concrete/Post.cs
@@ -1,4 +1,5 @@
 ﻿using Likegram.Core.Entities;
+using Likegram.Core.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace Likegram.Entities.Concrete
     public class Post : BaseEntity
     {
         public int UserId { get; set; }
+        public User User { get; set; }
+
         public List<PostComment> PostComments { get; set; }
         public string Description { get; set; }
         public List<PostLike> PostLikes { get; set; }
