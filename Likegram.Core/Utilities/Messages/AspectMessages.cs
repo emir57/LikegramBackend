bfec0d3ba2df448ca85775c946c2b72666684b47
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:10:48 +0300

    ValidationAspect

---
+++ Likegram.Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Core.Utilities.Messages
+{
+    public class AspectMessages
+    {
+        public static string UnvalidValidationType = "Geçersiz validasyon tipi";
+    }
+}
