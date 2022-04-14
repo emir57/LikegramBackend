9dc8d66571346d5cbf1ffe319209e1d0d187444b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:32:41 +0300

    IUserService

---
+++ Likegram.Business/Abstract/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using Likegram.Core.Entities.Concrete;
+using Likegram.Core.Utilities.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IResult> Add(User user);
+        Task<IResult> Update(User user);
+        Task<IResult> Delete(User user);
+
+        Task<IDataResult<User>> GetByEmail(string email);
+        Task<IDataResult<User>> GetByUsername(string userName);
+        Task<IDataResult<User>> GetById(int id);
+
+        Task<IDataResult<List<User>>> UserRoles(User user);
+    }
+}
