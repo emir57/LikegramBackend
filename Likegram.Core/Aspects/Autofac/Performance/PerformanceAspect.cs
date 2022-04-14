9436c049bf5be27259e4424fb4c05b3a1d50f631
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 19:15:38 +0300

    PerformanceAspect

---
+++ Likegram.Core/Aspects/Autofac/Performance/PerformanceAspect.cs
@@ -0,0 +1,37 @@
+﻿using Likegram.Core.Utilities.Interceptors;
+using Likegram.Core.Utilities.IoC;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Castle.DynamicProxy;
+
+namespace Likegram.Core.Aspects.Autofac.Performance
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private int _interval;
+        private Stopwatch _stopwatch;
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwatch = ServiceTool.Provider.GetService<Stopwatch>();
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _stopwatch.Start();
+        }
+        protected override void OnAFter(IInvocation invocation)
+        {
+            _stopwatch.Stop();
+            if(_stopwatch.Elapsed.TotalSeconds > _interval)
+            {
+                Debug.WriteLine($"{invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwatch.Elapsed.TotalSeconds} seconds");
+            }
+            _stopwatch.Reset();
+        }
+    }
+}
