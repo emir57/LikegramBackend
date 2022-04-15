44a9c17f8813b9c31537577ee670f3af64f53a94
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-15 23:13:24 +0300

    FollowUser

---
+++ Likegram.Entities/Concrete/FollowUser.cs
@@ -0,0 +1,21 @@
+﻿using Likegram.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class FollowUser
+    {
+        public int Id { get; set; }
+        [ForeignKey("FollowingUserId")]
+        //Takip Eden
+        public User FollowingUserId { get; set; }
+        [ForeignKey("FollowedUserId")]
+        //Takip Edilen
+        public User FollowedUserId { get; set; }
+    }
+}
