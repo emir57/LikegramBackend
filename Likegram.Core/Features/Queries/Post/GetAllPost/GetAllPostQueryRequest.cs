71b2276ae6b57710e6ebb3a4b92929d616edc0af
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 22:45:39 +0300

    Create GetAllPostQueryRequest

---
+++ Likegram.Core/Features/Queries/Post/GetAllPost/GetAllPostQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Core.Features.Queries.Post.GetAllPost
+{
+    public class GetAllPostQueryRequest : IRequest<GetAllPostQueryResponse>
+    {
+        public int FollowingUserId { get; set; }
+    }
+}
