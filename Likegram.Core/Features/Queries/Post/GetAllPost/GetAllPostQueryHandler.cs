5c021ba92c754013442fbe367d329f60a455c675
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 22:46:53 +0300

    Create GetAllPostQueryHandler

---
+++ Likegram.Core/Features/Queries/Post/GetAllPost/GetAllPostQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Likegram.Core.Features.Queries.Post.GetAllPost
+{
+    public class GetAllPostQueryHandler : IRequestHandler<GetAllPostQueryRequest, GetAllPostQueryResponse>
+    {
+        public Task<GetAllPostQueryResponse> Handle(GetAllPostQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
