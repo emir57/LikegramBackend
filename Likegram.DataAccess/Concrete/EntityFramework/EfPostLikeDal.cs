488f74e68f547747571a3875e43163ac6d19e608
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-13 18:02:21 +0300

    EfPostLikeDal

--- Likegram.DataAccess/Concrete/EntityFramework/EfPostLikeDal.cs
+++ Likegram.DataAccess/Concrete/EntityFramework/EfPostLikeDal.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Likegram.DataAccess.Concrete.EntityFramework
 {
-    public class EfPostLikeDal
+    public class EfPostLikeDal : EFEntityRepostioryBase<PostLike, LikegramDbContext>, IPostLikeDal
     {
     }
 }
