123f0a94c3542bb2fdefa9ae4f59e3238c2cb765
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-13 21:25:22 +0300

    DateTime is nullable

--- Likegram.Core/Entities/BaseEntity.cs
+++ Likegram.Core/Entities/BaseEntity.cs
@@ -9,8 +9,8 @@ namespace Likegram.Core.Entities
     public class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
-        public DateTime DeletedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
