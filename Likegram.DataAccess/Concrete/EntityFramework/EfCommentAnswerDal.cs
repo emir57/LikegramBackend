30b6c5aab3cdacf4226813d660d6c61b362cbe31
emir57 <emir.gurbuz06@hotmail.com>
2022-06-15 16:08:26 +0300

    EfCommentAnswerDal  GetByCommentIdAsync return result

--- Likegram.DataAccess/Concrete/EntityFramework/EfCommentAnswerDal.cs
+++ Likegram.DataAccess/Concrete/EntityFramework/EfCommentAnswerDal.cs
@@ -2,6 +2,7 @@
 using Likegram.DataAccess.Abstract;
 using Likegram.DataAccess.Contexts;
 using Likegram.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,22 @@ namespace Likegram.DataAccess.Concrete.EntityFramework
 {
     public class EfCommentAnswerDal : EFEntityRepostioryBase<CommentAnswer, LikegramDbContext>, ICommentAnswerDal
     {
+        public async Task<CommentAnswer> GetByCommentIdAsync(int commentId)
+        {
+            using(var context = new LikegramDbContext())
+            {
+                var result = from a in context.CommentAnswers
+                             where a.PostCommentId == commentId
+                             select new CommentAnswer
+                             {
+                                 Id = a.Id,
+                                 Answer = a.Answer,
+                                 CreatedDate = a.CreatedDate,
+                                 UpdatedDate = a.UpdatedDate,
+                                 DeletedDate = a.DeletedDate,
+                             };
+                return await result.ToListAsync();
+            }
+        }
     }
 }
