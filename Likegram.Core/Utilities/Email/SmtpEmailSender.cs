defe143d0c45ef571a4642dca201d057fa29338f
emir57 <emir.gurbuz06@hotmail.com>
2022-06-15 17:33:45 +0300

    SmtpEmailSender

--- Likegram.Core/Utilities/Email/SmtpEmailSender.cs
+++ Likegram.Core/Utilities/Email/SmtpEmailSender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,19 +11,19 @@ namespace Likegram.Core.Utilities.Email
 {
     public class SmtpEmailSender : IEmailService
     {
-        private string _username = "name";
-        private string _password = "password";
-        private int _port = 587;
-        private bool _enableSSL = true;
-        private string _host = "smtp-mail.outlook.com";
+        private readonly EmailSettings _emailSettings;
+        public SmtpEmailSender(IConfiguration configuration)
+        {
+            _emailSettings = configuration.GetSection("EmailSettings").Get<EmailSettings>();
+        }
         public Task SendMailAsync(string to, string title, string body)
         {
-            var client = new SmtpClient(_host, _port)
+            var client = new SmtpClient(_emailSettings.Host, _emailSettings.Port)
             {
-                Credentials = new NetworkCredential(_username, _password),
-                EnableSsl = _enableSSL,
+                Credentials = new NetworkCredential(_emailSettings.Username, _emailSettings.Password),
+                EnableSsl = _emailSettings.EnableSSL,
             };
-            return client.SendMailAsync(new MailMessage(_username, to, title, body)
+            return client.SendMailAsync(new MailMessage(_emailSettings.Username, to, title, body)
             {
                 IsBodyHtml = true
             });
