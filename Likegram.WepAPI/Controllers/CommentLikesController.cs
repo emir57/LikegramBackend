43ff1c5b437994426ab1fe7494243239a7155533
emir57 <emir.gurbuz06@hotmail.com>
2022-06-15 14:39:19 +0300

    Create CommentLikesController

---
+++ Likegram.WepAPI/Controllers/CommentLikesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Likegram.WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentLikesController : ControllerBase
+    {
+    }
+}
