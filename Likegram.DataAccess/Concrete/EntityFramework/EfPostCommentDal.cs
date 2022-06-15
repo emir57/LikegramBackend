b313eef92672677f42243004dcccc8c6d02549cf
Adnan Kaan ERBÖCÜ <98228678+QaanErbc@users.noreply.github.com>
2022-06-15 14:21:54 +0300

    EfPostCommentDal -> GetCommentsByPostId

--- Likegram.DataAccess/Concrete/EntityFramework/EfPostCommentDal.cs
+++ Likegram.DataAccess/Concrete/EntityFramework/EfPostCommentDal.cs
@@ -2,6 +2,7 @@
 using Likegram.DataAccess.Abstract;
 using Likegram.DataAccess.Contexts;
 using Likegram.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,24 @@ namespace Likegram.DataAccess.Concrete.EntityFramework
 {
     public class EfPostCommentDal : EFEntityRepostioryBase<PostComment, LikegramDbContext>, IPostCommentDal
     {
+        public async Task<List<PostComment>> GetCommentsByPostId(int postId)
+        {
+            using (var context = new LikegramDbContext())
+            {
+                var result = from c in context.PostComments
+                             where c.PostId == postId
+                             select new PostComment
+                             {
+                                 Id = c.Id,
+                                 PostId = postId,
+                                 Comment = c.Comment,
+                                 User = context.Users.SingleOrDefault(x => x.Id == c.UserId),
+                                 CreatedDate = c.CreatedDate,
+                                 UpdatedDate = c.UpdatedDate,
+                                 DeletedDate = c.DeletedDate,
+                             };
+                return await result.ToListAsync();
+            }
+        }
     }
 }
