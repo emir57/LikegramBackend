a66bcb08fb428e6f40abf70ac8ea2b3f6abdadef
emir57 <emir.gurbuz06@hotmail.com>
2022-06-17 14:26:56 +0300

    LikeOrUnLike use FEntity

--- Likegram.WepAPI/Controllers/PostLikesController.cs
+++ Likegram.WepAPI/Controllers/PostLikesController.cs
@@ -1,4 +1,6 @@
-﻿using Likegram.Business.Abstract;
+﻿using FluentEntity_ConsoleApp.FEntity;
+using Likegram.Business.Abstract;
+using Likegram.Entities.Concrete;
 using Likegram.WepAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,15 +26,15 @@ namespace Likegram.WepAPI.Controllers
                 var result2 = await _postLikesService.DeleteAsync(result.Data);
                 return Ok(result2);
             }
-            var result3 = await _postLikesService.AddAsync(new Entities.Concrete.PostLike
-            {
-                PostId = likeOrUnlikeViewModel.PostId,
-                UserId = likeOrUnlikeViewModel.UserId
-            });
+            PostLike postLike = new FluentEntity<PostLike>()
+                .AddParameter(p => p.PostId, likeOrUnlikeViewModel.PostId)
+                .AddParameter(p => p.UserId, likeOrUnlikeViewModel.UserId)
+                .GetEntity();
+            var result3 = await _postLikesService.AddAsync(postLike);
             return Ok(result3);
         }
         [HttpGet("checklike")]
-        public async Task<IActionResult> CheckLike([FromQuery]PostCheckLikeViewModel checkLikeViewModel)
+        public async Task<IActionResult> CheckLike([FromQuery] PostCheckLikeViewModel checkLikeViewModel)
         {
             var result = await _postLikesService.CheckLike(checkLikeViewModel.UserId, checkLikeViewModel.PostId);
             return Ok(result);
