1f17a922e0558e11721e28ff7ccf151b643d9d17
emir57 <emir.gurbuz06@hotmail.com>
2022-06-18 18:46:33 +0300

    inject IFavouritePostService

--- Likegram.WepAPI/Controllers/FavouritePostsController.cs
+++ Likegram.WepAPI/Controllers/FavouritePostsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Likegram.Business.Abstract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Likegram.WepAPI.Controllers
@@ -7,5 +8,11 @@ namespace Likegram.WepAPI.Controllers
     [ApiController]
     public class FavouritePostsController : ControllerBase
     {
+        private readonly IFavouritePostService _favouritePostService;
+
+        public FavouritePostsController(IFavouritePostService favouritePostService)
+        {
+            _favouritePostService = favouritePostService;
+        }
     }
 }
