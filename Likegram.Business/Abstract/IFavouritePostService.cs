2fb3ac294e024315e1acccc635015b6ed1182786
emir57 <emir.gurbuz06@hotmail.com>
2022-06-18 18:33:27 +0300

    IFavouritePostService

---
+++ Likegram.Business/Abstract/IFavouritePostService.cs
@@ -0,0 +1,21 @@
+﻿using Likegram.Core.Utilities.Result;
+using Likegram.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Business.Abstract
+{
+    public interface IFavouritePostService
+    {
+        Task<IResult> AddAsync(FavouritePost favouritePost);
+        Task<IResult> UpdateAsync(FavouritePost favouritePost);
+        Task<IResult> DeleteAsync(FavouritePost favouritePost);
+        Task<IDataResult<FavouritePost>> GetByIdAsync(int id);
+        Task<IDataResult<List<FavouritePost>>> GetListByUserId(int userId);
+        Task<IDataResult<Post>> GetListByUserIdAndPostId(int userId,int postId);
+        Task<IDataResult<List<FavouritePost>>> GetList();
+    }
+}
