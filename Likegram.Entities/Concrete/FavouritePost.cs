7b98549160243d2334cb4ed8f86eb9d8cbf9eed1
emir57 <emir.gurbuz06@hotmail.com>
2022-06-18 17:53:00 +0300

    FavouritePost Add Properties

--- Likegram.Entities/Concrete/FavouritePost.cs
+++ Likegram.Entities/Concrete/FavouritePost.cs
@@ -1,6 +1,8 @@
 ﻿using Likegram.Core.Entities;
+using Likegram.Core.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,12 @@ namespace Likegram.Entities.Concrete
 {
     public class FavouritePost : BaseEntity
     {
+        public int? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public int? PostId { get; set; }
+        [ForeignKey("PostId")]
+        public Post Post { get; set; }
     }
 }
