bf518e44961c324f34570840fe57439004e5ff93
emir57 <emir.gurbuz06@hotmail.com>
2022-06-18 17:51:32 +0300

    Create PostFavourite Entity

---
+++ Likegram.Entities/Concrete/FavouritePost.cs
@@ -0,0 +1,13 @@
+﻿using Likegram.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likegram.Entities.Concrete
+{
+    public class FavouritePost : BaseEntity
+    {
+    }
+}
