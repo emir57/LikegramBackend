105d14c30ec9c882b44d49c6edee82ae13b5e788
emir57 <emir.gurbuz06@hotmail.com>
2022-07-07 14:31:09 +0300

    IFollowUserService add operations

---
+++ Likegram.Business/Abstract/IFollowUserService.cs
@@ -0,0 +1,17 @@
+﻿using Likegram.Core.Utilities.Result;
+using Likegram.Entities.Concrete;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Likegram.Business.Abstract
+{
+    public interface IFollowUserService
+    {
+        Task<IResult> AddAsync(FollowUser followUser);
+        Task<IResult> DeleteAsync(FollowUser followUser);
+        Task<IDataResult<FollowUser>> GetByIdAsync(int id);
+        Task<IDataResult<List<FollowUser>>> GetListAsync();
+        Task<IDataResult<List<FollowUser>>> GetListByFollowedUserIdAsync(int followedUserId);
+        Task<IDataResult<List<FollowUser>>> GetListByFollowingUserIdAsync(int followingUserId);
+    }
+}
