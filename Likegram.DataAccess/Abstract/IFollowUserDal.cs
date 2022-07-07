978bf6bf34b53886594edc67ad0fb45ca42877aa
emir57 <emir.gurbuz06@hotmail.com>
2022-07-07 14:25:46 +0300

    Create IFollowUserDal

---
+++ Likegram.DataAccess/Abstract/IFollowUserDal.cs
@@ -0,0 +1,9 @@
+﻿using Likegram.Core.DataAccess;
+using Likegram.Entities.Concrete;
+
+namespace Likegram.DataAccess.Abstract
+{
+    public interface IFollowUserDal : IEntityRepository<FollowUser>
+    {
+    }
+}
