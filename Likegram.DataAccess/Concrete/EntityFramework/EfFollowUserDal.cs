198dc0e5dec0336d464fcb84e8f41cdafaa2f08d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-07 14:26:39 +0300

    Create EfFollowUserDal

---
+++ Likegram.DataAccess/Concrete/EntityFramework/EfFollowUserDal.cs
@@ -0,0 +1,11 @@
+﻿using Likegram.Core.DataAccess.Entityframework;
+using Likegram.DataAccess.Abstract;
+using Likegram.DataAccess.Contexts;
+using Likegram.Entities.Concrete;
+
+namespace Likegram.DataAccess.Concrete.EntityFramework
+{
+    public class EfFollowUserDal : EFEntityRepostioryBase<FollowUser, LikegramDbContext>, IFollowUserDal
+    {
+    }
+}
